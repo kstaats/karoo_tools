42655fa4769fde9d1180e633f1634bd1dffb341e
kstaats <github@overthesun.com>
2018-04-19 09:12:06 -0700

    added regression option

--- karoo_pipeline.py
+++ karoo_pipeline.py
@@ -1,7 +1,7 @@
 # Karoo GP Pipeline
 # by Kai Staats; see LICENSE.md
 # see LICENSE.md
-# version 2018 03/26
+# version 2018 04/19
 
 import os
 import csv
@@ -55,6 +55,14 @@ while True:
 	except ValueError: print '\n\t\033[32m An empty expression is not going to get you very far. Try again ...\n\033[0;0m'
 	except KeyboardInterrupt: sys.exit()
 
+while True:
+	try:
+		function = raw_input('\t Select (c)lassification or (r)egression? (default c): ')
+		if function not in ['c','r','']: raise ValueError()
+		function = function or 'c'; break
+	except ValueError: print '\t\033[32m Select from the options given. Try again ...\n\033[0;0m'
+	except KeyboardInterrupt: sys.exit()
+
 #while True:
 #	try:
 #		print_out = raw_input('\n\t Print the results to screen? y/n (default n): ')
@@ -62,7 +70,7 @@ while True:
 #		print_out = print_out or 'n'; break
 #	except ValueError: print '\033[32mSelect from the options given. Try again ...\n\033[0;0m'
 
-precision = 4 # set the number of floating points
+precision = 6 # set the number of floating points
 
 
 ### LOAD DATA ###
@@ -82,9 +90,7 @@ for row in range(0, data_rows): # increment through each row of data
 		data_dict.update( {header[col]:data[row,col]} ) # add a row of data into a dictionary	
 		
 	data_dict_array = np.append(data_dict_array, data_dict.copy()) # add a dictionary of data into the array
-
-		
-### CLASSIFY THE PIPELINE DATA ###
+	
 
 def fx_eval_subs(datum, algo_paste):
 
@@ -92,6 +98,12 @@ def fx_eval_subs(datum, algo_paste):
 	Evaluation of the raw expression into a sympified expression was already done in Karoo GP. Therefore, we need only 
 	assign each feature (column) with an associated data point (row) from the pipeline data, and then process.
 	'''
+	
+	# If using iPython, after the pipeline is executed, you can extract any given feature value from any given row:
+	#
+	#		data_dict_array[row]['key']
+	# 
+	# ... where 'key' is a column header (feature variable)
 	
 	algo_sym = sp.sympify(algo_paste) # string converted to a functional expression
 	subs = algo_sym.subs(datum) # process the expression against the datum
@@ -103,45 +115,65 @@ def fx_eval_subs(datum, algo_paste):
 	return result
 	
 
-# If using iPython, after the pipeline is executed, you can extract any given feature value from any given row:
-#
-#		data_dict_array[row]['key']
-# 
-# ... where 'key' is a column header (feature variable)
-
 class_0 = []
 class_1 = []
+regress = []
 
-for row in range(0, data_rows):
-	result = fx_eval_subs(data_dict_array[row], algo_paste) # process the expression against the test data
+### CONDUCT A CLASSIFICATION RUN ###
+
+if function == 'c':
+
+	for row in range(0, data_rows):
+		result = fx_eval_subs(data_dict_array[row], algo_paste) # process the expression against the test data
 	
-	if result <= 0: # test for class 0
-		if header[0] == 'id': class_0.append([data_dict_array[row]['id']]) # record the ID if available
-		else: class_0.append(row) # else record the .csv row number
-		print '\t\033[36m data row', row, 'predicts class:\033[1m 0 as', result, '<=', 0, '\033[0;0m'
+		if result <= 0: # test for class 0
+			if header[0] == 'id': class_0.append([data_dict_array[row]['id']]) # record the ID if available
+			else: class_0.append(row) # else record the .csv row number
+			print '\t\033[36m data row', row, 'predicts class:\033[1m 0 as', result, '<=', 0, '\033[0;0m'
 		
-	elif result > 0: # test for class 1
-		if header[0] == 'id': class_1.append([data_dict_array[row]['id']]) # record the ID if available		
-		else: class_1.append(row) # else record the .csv row number
-		print '\t\033[36m data row', row, 'predicts class:\033[1m 1 as', result, '>', 0, '\033[0;0m'
+		elif result > 0: # test for class 1
+			if header[0] == 'id': class_1.append([data_dict_array[row]['id']]) # record the ID if available		
+			else: class_1.append(row) # else record the .csv row number
+			print '\t\033[36m data row', row, 'predicts class:\033[1m 1 as', result, '>', 0, '\033[0;0m'
 		
-	else: print 'Whoa! The result is neither <= 0 nor > 0. The universe will implode in 5 ... 4 ... 3 ...'; sys.exit()
+		else: print 'Whoa! The result is neither <= 0 nor > 0. The universe will implode in 5 ... 4 ... 3 ...'; sys.exit()
 	
+
+### CONDUCT A REGRESSION RUN ###
+
+else:
+
+	for row in range(0, data_rows):
+		result = fx_eval_subs(data_dict_array[row], algo_paste) # process the expression against the test data
+		regress.append(result) # else record the .csv row number
+		print '\t\033[36m data row', row, 'produces:\033[1m', result, '\033[0;0m'
+		
 
 ### SAVE THE OUTPUT ###
 
-print '\n\t Writing', len(class_1), 'class 1 to disk'
-target = open('class_1.csv', 'w'); target.close() # initialise class 1 .csv file
-with open('class_1.csv', 'a') as csv_file:
-	target = csv.writer(csv_file, delimiter=',')
-	#target.writerows([['IDs for class 1 (label 1)']])
-	target.writerows([class_1])
+if function == 'c':
 
-print '\n\t Writing', len(class_0), 'class 0 to disk'
-target = open('class_0.csv', 'w'); target.close() # initialise class 0 .csv file
-with open('class_0.csv', 'a') as csv_file:
-	target = csv.writer(csv_file, delimiter=',')
-	#target.writerows([['IDs for class 0 (label 0)']])
-	target.writerows([class_0])
+	print '\n\t Writing', len(class_1), 'class 1 to disk'
+	target = open('class_1.csv', 'w'); target.close() # initialise class 1 .csv file
+	with open('class_1.csv', 'a') as csv_file:
+		target = csv.writer(csv_file, delimiter=',')
+		#target.writerows([['IDs for class 1 (label 1)']])
+		target.writerows([class_1])
+
+	print '\n\t Writing', len(class_0), 'class 0 to disk'
+	target = open('class_0.csv', 'w'); target.close() # initialise class 0 .csv file
+	with open('class_0.csv', 'a') as csv_file:
+		target = csv.writer(csv_file, delimiter=',')
+		#target.writerows([['IDs for class 0 (label 0)']])
+		target.writerows([class_0])
+
+
+else:
+
+	print '\n\t Writing', len(class_1), 'output to disk'
+	target = open('regress.csv', 'w'); target.close() # initialise regression file
+	with open('regress.csv', 'a') as csv_file:
+		target = csv.writer(csv_file, delimiter='\n')
+		target.writerows([regress])
 
 
